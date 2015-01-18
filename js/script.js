e6487884393a08e7e1315f54dd7738c8f5d1b89f
Raphael <mfettaya@hotmail.com>
2015-01-18 12:24:26 +0100

    Fixed bugs

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@
 //Billeterie 1 correspond jour 1 sans camping 2 jour 1 avec camping
 $(document).ready(function(){
 	var id_b= 0;
+  $('#c1').unbind('click',false);                // Desactive le click sur la case camping
 	$('#day1').click(function(){
     console.log('Le un'+id_b);
        if(id_b%2==0 && id_b>0){
           $('#day2').removeClass('active');
           $('#day1').addClass('active');
-          document.getElementById('d1').click();
+          $('#d2').prop('checked',false);
+          $('#d1').prop('checked', true);
           if(id_b==4){
             $('.pass2').fadeOut(500,function(){$('.pass1c').fadeIn(1000);});
           	id_b=3;
@@ -19,7 +21,7 @@ $(document).ready(function(){
        }
        if (id_b==0){
        	  $('#day1').addClass('active');
-       	  document.getElementById('d1').click();
+       	  $('#d1').prop('checked', true);
           $('.pass1').fadeIn(1000);
        	  id_b=1;
        }
@@ -29,8 +31,10 @@ $(document).ready(function(){
        if(id_b%2==1 && id_b>0){                     // Cas cliqué sur 1 jour
           $('#day1').removeClass('active');
           $('#day2').addClass('active');
-          document.getElementById('d2').click();    // Cocher automatiquement la box
+          $('#d1').prop('checked',false);
+          $('#d2').prop('checked', true);    // Cocher automatiquement la box
           if(id_b==3){                              // Cas camping
+            console.log('here');
             $('.pass1c').fadeOut(500,function(){$('.pass2').fadeIn(1000);});    // On enleve pass2 et quand l'action se termine on affiche pass1
           	id_b=4;                                
           }
@@ -41,7 +45,7 @@ $(document).ready(function(){
        }
        if (id_b==0){                                //Cas premier clique
        	  $('#day2').addClass('active'); 
-          document.getElementById('d2').click();
+          $('#d2').prop('checked', true);
           $('.pass2').fadeIn(1000);
        	  id_b=2;
        }
@@ -49,17 +53,17 @@ $(document).ready(function(){
     $('#camp').click(function(){
       if(id_b<3 && id_b!=0){
         $('#camp').addClass('active');
-        document.getElementById('c1').click();
+        $('#c1').prop('checked', true);
         if(id_b==1){                              //Probleme avec le clique le if
             id_b=3;
             $('.pass1').fadeOut(500,function(){$('.pass1c').fadeIn(1000);});
         }
-        if(id_b==2)                          
+        if(id_b==2)                         
             id_b=4
       }
       else if(id_b>2){
         $('#camp').removeClass('active');
-        document.getElementById('c1').click();
+        $('#c1').prop('checked', false);
         if(id_b==3){                              //Probleme avec le clique le if
             id_b=1;
             $('.pass1c').fadeOut(500,function(){$('.pass1').fadeIn(1000);});
@@ -69,9 +73,9 @@ $(document).ready(function(){
       }
       else{
         $('#camp').addClass('active');
-        document.getElementById('c1').click();
+        $('#c1').attr('checked', true);
+        $('#d1').attr('checked', true);
         $('#day1').addClass('active');
-        document.getElementById('d1').click();
         id_b=3;
       }
     });
