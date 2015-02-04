b57c29ae115736de88d631184d9b6bee4d93cf98
Ruben Sebbane <ruben.sebbane@gmail.com>
2015-02-04 15:55:50 +0100

    PANO

--- js/app.js
+++ js/app.js
@@ -1,62 +1,70 @@
 var app = angular.module("Panorama", []);
 app.controller("artistCtrl",function($scope){
-	 $scope.artistes=[
-	 	{
-	 		name:'KAYTRANADA',
-	 		jour:'VENDREDI 3 AVRIL',
-	 		heure:'23h20',
-	 		style:'AUCUN/STYLE',
-	 		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis fermentum enim, ut pulvinar lacus interdum vitae. Curabitur ultrices, nibh in auctor tincidunt, neque sapien fermentum dolor, in convallis augue quam vel mauris. Duis mollis sollicitudin nulla placerat fermentum. Nunc ipsum ex, condimentum a ante sed, congue pulvinar leo. Cras dignissim, justo ut ultrices ultricies, mauris ligula malesuada leo, fermentum varius purus nulla volutpat est. Duis purus eros, dictum sit amet commodo in, ullamcorper a magna. Ut sit amet feugiat ligula. Sed sagittis massa dignissim, convallis nisi nec, placerat erat.',
-	 		club:'Grand Hall',
-	 		pic_link_black:'images/artistes/kay.jpg',
-	 		pic_link_color:'images/artistes/kay_c.jpg',
-	 		vid_link:'https://www.youtube.com/watch?v=z1BVZ0PMs9M',
-	 		twitter:'#',
-	 		facebook:'#',
-	 		soundcloud:'#'
-	 	},	 	
-	    {
-	 		name:'KAYTRANADA',
-	 		jour:'VENDREDI 3 AVRIL',
-	 		heure:'23h20',
-	 		style:'AUCUN/STYLE',
-	 		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis fermentum enim, ut pulvinar lacus interdum vitae. Curabitur ultrices, nibh in auctor tincidunt, neque sapien fermentum dolor, in convallis augue quam vel mauris. Duis mollis sollicitudin nulla placerat fermentum. Nunc ipsum ex, condimentum a ante sed, congue pulvinar leo. Cras dignissim, justo ut ultrices ultricies, mauris ligula malesuada leo, fermentum varius purus nulla volutpat est. Duis purus eros, dictum sit amet commodo in, ullamcorper a magna. Ut sit amet feugiat ligula. Sed sagittis massa dignissim, convallis nisi nec, placerat erat.',
-	 		club:'Grand Hall',
-	 		pic_link_black:'images/artistes/kay.jpg',
-	 		pic_link_color:'images/artistes/kay_c.jpg',
-	 		vid_link:'https://www.youtube.com/watch?v=z1BVZ0PMs9M',
-	 		twitter:'#',
-	 		facebook:'#',
-	 		soundcloud:'#'
-	 	},
-	 ];
+	$scope.artistes=[
+	{
+		name:'KAYTRANADA',
+		jour:'VENDREDI 3 AVRIL',
+		heure:'23h20',
+		style:'AUCUN/STYLE',
+		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis fermentum enim, ut pulvinar lacus interdum vitae. Curabitur ultrices, nibh in auctor tincidunt, neque sapien fermentum dolor, in convallis augue quam vel mauris. Duis mollis sollicitudin nulla placerat fermentum. Nunc ipsum ex, condimentum a ante sed, congue pulvinar leo. Cras dignissim, justo ut ultrices ultricies, mauris ligula malesuada leo, fermentum varius purus nulla volutpat est. Duis purus eros, dictum sit amet commodo in, ullamcorper a magna. Ut sit amet feugiat ligula. Sed sagittis massa dignissim, convallis nisi nec, placerat erat.',
+		club:'Grand Hall',
+		pic_link_black:'images/artistes/kay.jpg',
+		pic_link_color:'images/artistes/kay_c.jpg',
+		vid_link:'https://www.youtube.com/watch?v=z1BVZ0PMs9M',
+		twitter:'#',
+		facebook:'#',
+		soundcloud:'#'
+	},	 	
+	{
+		name:'KAYTRANADA',
+		jour:'VENDREDI 3 AVRIL',
+		heure:'23h20',
+		style:'AUCUN/STYLE',
+		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis fermentum enim, ut pulvinar lacus interdum vitae. Curabitur ultrices, nibh in auctor tincidunt, neque sapien fermentum dolor, in convallis augue quam vel mauris. Duis mollis sollicitudin nulla placerat fermentum. Nunc ipsum ex, condimentum a ante sed, congue pulvinar leo. Cras dignissim, justo ut ultrices ultricies, mauris ligula malesuada leo, fermentum varius purus nulla volutpat est. Duis purus eros, dictum sit amet commodo in, ullamcorper a magna. Ut sit amet feugiat ligula. Sed sagittis massa dignissim, convallis nisi nec, placerat erat.',
+		club:'Grand Hall',
+		pic_link_black:'images/artistes/kay.jpg',
+		pic_link_color:'images/artistes/kay_c.jpg',
+		vid_link:'https://www.youtube.com/watch?v=z1BVZ0PMs9M',
+		twitter:'#',
+		facebook:'#',
+		soundcloud:'#'
+	},
+	];
 });
 
 
 app.controller("PanoCtrl",function($scope) {
-    $scope.artiste = [
-        {'name': 'KAYTRANADA', 'id':'1', 'heure':'19h30', 'isChecked' : false},
-        {'name': 'TEST', 'id':'2', 'heure':'20h30', 'isChecked' : false},
-        {'name': 'TEST2', 'id':'3', 'heure':'21h30', 'isChecked' : false}];
-    
-    $scope.artisteIncludes = [];
-    
-    $scope.includeArtiste = function(artisteID) {
-        var i = $.inArray(artisteID, $scope.artisteIncludes);
-        if (i > -1) {
-            $scope.artisteIncludes.splice(i, 1);
-        } else {
-            $scope.artisteIncludes.push(artisteID);
-        }
-    
-    } 
-    
-    $scope.artisteFilter = function(artiste) {
-        if ($scope.artisteIncludes.length >= 0) {
-            if ($.inArray(artiste.id, $scope.artisteIncludes) < 0)
-                return;
-        }
-        
-        return artiste;
-    }     
+	$scope.artiste = [
+	{'name': 'KAYTRANADA', 'id':'1', 'jour' : '1', 'heure':'19h30', 'isChecked' : false},
+	{'name': 'TEST', 'id':'2', 'jour' : '2', 'heure':'20h30', 'isChecked' : false},
+	{'name': 'TEST2', 'id':'3', 'jour' : '3', 'heure':'21h30', 'isChecked' : false}];
+
+	$scope.artisteIncludes = [];
+
+	$scope.includeArtiste = function(artisteID) {
+		var i = $.inArray(artisteID, $scope.artisteIncludes);
+		if (i > -1) {
+			$scope.artisteIncludes.splice(i, 1);
+		} else {
+			$scope.artisteIncludes.push(artisteID);
+		}
+
+	} 
+
+	$scope.artisteFilter = function(artiste) {
+		if ($scope.artisteIncludes.length >= 0) {
+			if ($.inArray(artiste.id, $scope.artisteIncludes) < 0)
+				return;
+		}
+
+		return artiste;
+	}
+
+	$scope.dayFilter = function(artiste) {
+		if (artiste.jour == $scope.day_pano)
+			return artiste;
+		else
+			return;
+
+	}     
 });
